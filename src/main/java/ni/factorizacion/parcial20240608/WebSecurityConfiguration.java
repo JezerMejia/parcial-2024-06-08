a9ea4fb3d5c523e93f7cba3009d75631d2a20cd0
Jezer Mejía <amyuki4@icloud.com>
2024-06-08 02:33:36 -0600

    Add CorsConfiguration

--- src/main/java/ni/factorizacion/parcial20240608/WebSecurityConfiguration.java
+++ src/main/java/ni/factorizacion/parcial20240608/WebSecurityConfiguration.java
@@ -16,6 +16,11 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -60,6 +65,17 @@ public class WebSecurityConfiguration {
                 .passwordEncoder(passwordEncoder);
 
         return managerBuilder.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
