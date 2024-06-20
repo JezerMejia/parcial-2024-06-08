5bf94fd397ac637cabef1e9bb796ddaf391e9b34
Jezer Mejía <92834478+JezerMejia@users.noreply.github.com>
2024-06-20 07:26:14 -0600

    Add RejectAppointment modal (#25)
    Co-authored-by: Jezer Mejía <amyuki4@icloud.com>

--- client/src/components/Modal/ModalComponent.vue
+++ client/src/components/Modal/ModalComponent.vue
@@ -39,7 +39,10 @@ onUnmounted(() => {
     class="fixed left-0 top-0 z-10 hidden h-dvh w-dvw place-items-center bg-black/50 open:grid"
     @click="close()"
   >
-    <div class="overflow-hidden rounded-lg border border-blue-300 bg-white" @click.stop="">
+    <div
+      class="w-[480px] overflow-hidden rounded-lg border border-blue-300 bg-white"
+      @click.stop=""
+    >
       <slot></slot>
     </div>
   </dialog>
