b3ed81c5796de0f74cc2f4584460c61e753f8e04
Jezer Mejía <amyuki4@icloud.com>
2024-06-20 09:41:13 -0600

    Add findMedics and findPatients

--- src/main/java/ni/factorizacion/parcial20240608/repositories/UserRepository.java
+++ src/main/java/ni/factorizacion/parcial20240608/repositories/UserRepository.java
@@ -2,10 +2,19 @@ package ni.factorizacion.parcial20240608.repositories;
 
 import ni.factorizacion.parcial20240608.domain.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
     User findByEmail(String email);
+
     User findByUsername(String username);
+
+    @Query("SELECT u from User u JOIN u.roles r WHERE r.id = 'DOCT'")
+    List<User> findMedics();
+
+    @Query("SELECT u from User u JOIN u.roles r WHERE r.id = 'PTNT'")
+    List<User> findPatients();
 }
