69bd02ff1815dd9df42b2fb3bba1167a01b2790a
Mrsty21 <165355191+Mrsty21@users.noreply.github.com>
2024-06-20 04:35:02 -0600

    Home menu (#17)
    * fsdfas
    
    * home menu made

---
+++ client/src/components/SquareButton.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import VueFeather from "vue-feather";
+
+const props = defineProps<{
+  title: string;
+  href: string;
+  icon: "folder" | "settings" | "eye" | "calendar";
+}>();
+</script>
+<template>
+  <div>
+    <a class="flex rounded-md flex-col items-center justify-center bg-white size-80 text-blue-400 hover:bg-blue-400 hover:text-white transition-all gap-6 p-4 active:scale-95 hover:shadow-glow"
+      :href="props.href">
+      <VueFeather :type="props.icon" class="size-[7.5rem]" />
+      <span class="font-medium text-xl">{{ props.title }}</span>
+    </a>
+  </div>
+</template>
