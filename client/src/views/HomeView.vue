a9efaf43209fc3fbdfea3003034c4227acd9e82d
davidquintr <davidquintr97@gmail.com>
2024-06-20 02:12:40 -0600

    added forms

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
+  import DateTimeForm from '@/components/Forms/DateTimeForm.vue';
+import InputForm from '@/components/Forms/InputForm.vue';
 </script>
 <template>
   <h1 class="text-4xl">Hola</h1>
+  <div class="bg-white p-8">
+    <DateTimeForm title="Testing" />
+    <InputForm title="Testing" />
+  </div>
 </template>
