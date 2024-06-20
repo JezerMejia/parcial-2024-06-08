3b7a0f02db7e44d5473d82de096cad49cdf37d1b
davidquintr <davidquintr97@gmail.com>
2024-06-20 02:37:36 -0600

    added layout for non-login users

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
-  import DateTimeForm from '@/components/Forms/DateTimeForm.vue';
+import DateTimeForm from '@/components/Forms/DateTimeForm.vue';
 import InputForm from '@/components/Forms/InputForm.vue';
+import { ref } from 'vue';
+
+const testingInput = ref<string>()
+
+const handleInputUpdate = (value: string) => {
+  testingInput.value = value;
+}
+
 </script>
 <template>
   <h1 class="text-4xl">Hola</h1>
   <div class="bg-white p-8">
     <DateTimeForm title="Testing" />
-    <InputForm title="Testing" />
+    <InputForm @update:value="handleInputUpdate" title="Testing" />
   </div>
 </template>
