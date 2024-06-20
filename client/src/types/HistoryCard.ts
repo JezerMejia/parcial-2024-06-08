7f08adcc3629c3ec6f7c02fd0ca0e1e588a68768
LeoCorea <00542724@uca.edu.sv>
2024-06-20 16:55:48 -0600

    Added Types

---
+++ client/src/types/HistoryCard.ts
@@ -0,0 +1,9 @@
+import { ExecutionState as ExcecutionStateType } from "@/types/ExecutionState";
+
+export default interface History {
+    status: ExcecutionStateType;
+    username: string;
+    startDate: Date;
+    endDate: Date;
+  }
+  
