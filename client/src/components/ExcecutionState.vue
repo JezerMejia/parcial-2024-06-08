bad1b00d955230b469c9bbbc8af66bed725c8000
davidquintr <davidquintr97@gmail.com>
2024-06-20 04:03:08 -0600

    added excecution state component

---
+++ client/src/components/ExcecutionState.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ExecutionState } from "@/types/ExecutionState"
+
+const props = defineProps<{ state: ExecutionState }>()
+
+const background: {[key in ExecutionState] : string } = {
+    [ExecutionState.CANCELED]: "bg-red-200",
+    [ExecutionState.REJECTED]: "bg-red-200",
+    [ExecutionState.FINISHED]: "bg-green-200",
+    [ExecutionState.IN_EXC]: "bg-yellow-200",
+    [ExecutionState.PEND_APR]: "bg-blue-200",
+    [ExecutionState.PEND_EXC]: "bg-blue-200",
+}
+
+const text: {[key in ExecutionState] : string } = {
+    [ExecutionState.CANCELED]: "text-red-400",
+    [ExecutionState.REJECTED]: "text-red-400",
+    [ExecutionState.FINISHED]: "text-green-400",
+    [ExecutionState.IN_EXC]: "text-yellow-400",
+    [ExecutionState.PEND_APR]: "text-blue-400",
+    [ExecutionState.PEND_EXC]: "text-blue-400",
+}
+
+
+</script>
+<template>
+    <div class="p-2 text-center font-bold" :class="[background[props.state], text[props.state]]">
+        <span>{{ props.state }}</span>
+    </div>
+</template>
---
+++ client/src/types/ExecutionState.ts
@@ -0,0 +1,8 @@
+export enum ExecutionState {
+    PEND_APR = "Pendiente de Aprobación",
+    PEND_EXC = "Pendiente de Ejecución",
+    IN_EXC = "En Ejecución",
+    FINISHED = "Finalizado",
+    REJECTED = "Rechazado",
+    CANCELED = "Cancelado"
+}
