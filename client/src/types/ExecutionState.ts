a422c34c3d7b2e90c409cedeb2e3a6d8d4542481
LeoCorea <00542724@uca.edu.sv>
2024-06-21 01:21:52 -0600

    Fixing writing problems

--- client/src/types/ExecutionState.ts
+++ client/src/types/ExecutionState.ts
@@ -2,7 +2,7 @@ export enum ExecutionState {
   PEND_APR = "Pendiente de aprobación",
   PEND_EXC = "Pendiente de ejecución",
   IN_EXC = "En ejecución",
-  FINISHED = "Finalizado",
-  REJECTED = "Rechazado",
-  CANCELED = "Cancelado",
+  FINISHED = "Finalizada",
+  REJECTED = "Rechazada",
+  CANCELED = "Cancelada",
 }
