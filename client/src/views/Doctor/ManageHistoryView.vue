771259b8ce1b069cc33cfe8ec79983db53a71cd2
Jezer Mejía <amyuki4@icloud.com>
2024-06-21 16:53:35 -0600

    Remove add history button

--- client/src/views/Doctor/ManageHistoryView.vue
+++ client/src/views/Doctor/ManageHistoryView.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import type User from "@/types/User";
-import VueFeather from "vue-feather";
 import PatientCard from "@/components/Cards/PatientCard.vue";
 import { useAuthenticatedFetch } from "@/composables/useBaseFetch";
 import { onMounted, ref } from "vue";
@@ -28,14 +27,7 @@ onMounted(async () => {
 
 <template>
   <section v-if="isAllowed" class="flex w-full flex-col gap-6 rounded-lg bg-white p-4">
-    <CurrentPageInfo icon="grid" title="Gestionar Historial">
-      <button
-        class="mr-2 flex items-center gap-1 rounded-lg bg-blue-200 px-3 py-2 text-blue-400 transition-all active:scale-95"
-      >
-        <VueFeather type="plus" />
-        <span>Nuevo historial</span>
-      </button>
-    </CurrentPageInfo>
+    <CurrentPageInfo icon="grid" title="Gestionar Historial" />
     <ul class="grid gap-4 py-4 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
       <PatientCard v-for="(item, index) in users" :key="index" :patient="item" :asist="false" />
     </ul>
