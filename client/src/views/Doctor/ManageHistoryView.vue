38397d7b018f2ab4f09d9897f3ccd69af3938496
Jezer Mejía <92834478+JezerMejia@users.noreply.github.com>
2024-06-21 16:52:35 -0600

    Add CreateHistory modal (#44)
    Co-authored-by: Jezer Mejía <amyuki4@icloud.com>

--- client/src/views/Doctor/ManageHistoryView.vue
+++ client/src/views/Doctor/ManageHistoryView.vue
@@ -1,19 +1,3 @@
-<template>
-  <section v-if="isAllowed" class="flex w-full flex-col gap-6 rounded-lg bg-white p-4">
-    <CurrentPageInfo icon="grid" title="Gestionar Historial">
-      <button
-        class="mr-2 flex items-center gap-1 rounded-lg bg-blue-200 px-3 py-2 text-blue-400 transition-all active:scale-95"
-      >
-        <VueFeather type="plus" />
-        <span>Nuevo historial</span>
-      </button>
-    </CurrentPageInfo>
-    <ul class="grid gap-4 py-4 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
-      <PatientCard v-for="(item, index) in users" :key="index" :patient="item" :asist="false" />
-    </ul>
-  </section>
-  <ForbiddenAlert v-else />
-</template>
 <script setup lang="ts">
 import type User from "@/types/User";
 import VueFeather from "vue-feather";
@@ -41,3 +25,21 @@ onMounted(async () => {
   console.log(users.value);
 });
 </script>
+
+<template>
+  <section v-if="isAllowed" class="flex w-full flex-col gap-6 rounded-lg bg-white p-4">
+    <CurrentPageInfo icon="grid" title="Gestionar Historial">
+      <button
+        class="mr-2 flex items-center gap-1 rounded-lg bg-blue-200 px-3 py-2 text-blue-400 transition-all active:scale-95"
+      >
+        <VueFeather type="plus" />
+        <span>Nuevo historial</span>
+      </button>
+    </CurrentPageInfo>
+    <ul class="grid gap-4 py-4 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+      <PatientCard v-for="(item, index) in users" :key="index" :patient="item" :asist="false" />
+    </ul>
+  </section>
+
+  <ForbiddenAlert v-else />
+</template>
